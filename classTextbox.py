f85013c500640fdd57cbdd61dc3ef9681f23ac97
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2025-10-07 11:17:05 -0400

    Added class Textbox
    Added a class called Textbox in order to instantiate many textbox objects. Added code in main file to accommodate textboxes. Still need to validate user input in textboxes, debug, and polish.

---
+++ classTextbox.py
@@ -0,0 +1,77 @@
+''' CLASS for text input boxes, of the Matrix Calculator 
+    * used for matrix dimensions plus user manually entering matrix values
+    * allows for numerous instances of text boxes to be created 
+
+'''
+
+
+import pygame, pygame_textinput
+
+# is the textbox class; each textbox will be instantiated from this
+class Textbox:
+
+    # declares new Pygame Rectangle objects. the parameters are x-coord, y-coord, width, height, font, max length and allowed characters
+    # maxLength and allowedChars are needed because the user can only enter specific characters, and a specific amount of them
+    # maxLength will likely be 7 because there could be a 3-digit over a 3-digit number in a fraction
+    # allowedChars
+    def __init__(self, x, y, width, height, font, maxLength = None, allowedChars = None):
+        self.rect = pygame.Rect(x, y, width, height)
+        
+        # pygame_textinput has the TextInputVisualizer (visualizes text)
+        self.textinput =  pygame_textinput.TextInputVisualizer(font_object=font)
+
+        # Boolean to tell if a textbox object is currently active or not
+        self.active = False
+
+        # validation variable to ensure the user enters proper data
+        self.allowedChars = allowedChars
+    
+    # is called when an event occurs, like when the user clicks or presses a key
+    # also validates input when the user enters data
+    def handleEvents(self, events):
+        # IF a textbox is active, then it will update its text based on the event.
+        # for example, if textbox is active, then when the user presses a key that textbox updates with that key
+        if self.active:
+            self.textinput.update(events)
+
+        # validates input type
+        if self.allowedChars:
+            filteredText = ""
+            for char in self.textinput.value:
+                if char in self.allowedChars:
+                    filteredText += char 
+            self.textinput.value = filteredText
+        
+        # validates input length
+        if self.
+    # draws the textbox on the screen. the parameters go: surface, color, Rectangle object
+    # the textbox will have a colored border around it when it is active
+    '''
+    This is the flow of the "draw" function: 
+    * Draws background rectangle > draws border outline > blits text surface (self.textinput.surface) into textbox
+    * Displayed with the later "pygame.display.flip()"
+    '''
+    def draw(self, surface):
+        # draws the main textbox itself. fills it with white
+        pygame.draw.rect(surface, (255, 255, 255), self.rect)
+        
+        # sets the border color depending on if the textbox is active
+        if self.active:
+            color = (0, 255, 255)
+        else:
+            color = (150, 150, 150)
+        
+        # draws the textbox rectangle with a border width/stroke of 2 pixels, using the specified border color
+        pygame.draw.rect(surface, color, self.rect, 2)
+
+        # draws the data onto the screen. format goes as follows:
+        # "surface" is the target surface to copy ONTO. "self.textinput.surface" is the surface to copy FROM
+        # "self.textinput.surface" automatically creates a surface every frame from user events
+        # the last parameter is (x,y) coords of where to copy the data; in this case, the top left of the textbox
+        surface.blit(self.textinput.surface, (self.rect.x + 5, self.rect.y + 5) )
+
+    # returns Boolean based on if the user clicks on the textbox
+    def clickedInside(self, pos):
+        return self.rect.collidepoint(pos)
+
+
--- testUseUI.py
+++ testUseUI.py
@@ -1,5 +1,8 @@
 import pygame, pygame_textinput
 
+# imports needed files
+from classTextbox import Textbox
+
 ''' PYGAME INFORMATION
 
 * The origin (coords of 0,0) is the top left corner. Positive directions are right and down
@@ -36,7 +39,7 @@ MATRICES' dimensions are in the format of (rows) X (columns)
 for example, a 3x2 matrix has 3 rows and 2 columns
 '''
 
-# example code; replace with importing from other files
+# example code; replace with user entering dimensions in textboxes
 numColumns = 2
 numRows = 3
 
@@ -54,11 +57,14 @@ fontDimensions = pygame.font.Font(None, 40)
 textInput = pygame_textinput.TextInputVisualizer(font_color = (255, 100, 0), font_object = fontDimensions)
 # NOTE: access user input via textInput.value
 
+# creates some textboxes on the screen. these ones are static and will not move
+boxRows = Textbox(620, 60, 80, 40, fontDimensions)
+boxColumns = Textbox(720, 60, 80, 40, fontDimensions)
 
-# needs to click on box, and start typing to enter in that box. when clicked elsewhere, or enter, or escape, then exit typing
-activeTyping = False # if the user is actively typing
-inputColumns = "" # what the user inputs for number of columns
-inputRows = "" # user input for number of rows
+# creates a list that will store all matrix textboxes
+textboxes = []
+textboxes.append(boxRows)
+textboxes.append(boxColumns)
 
 # code needs to establish a matrix based on the dimensions provided by user
 # perhaps the width of the matrix itself shall stay the same;
@@ -70,9 +76,6 @@ inputRows = "" # user input for number of rows
 #       - the matrix resizes itself to fit the screen?
 
 # information for the rectangles and text - coords, dimensions, color
-# creates a list storing all textboxes
-listTextboxes = []
-activeTextbox = ""
 
 buttonMainCoords = (40, 40, 160, 80) # x-coord, y-coord, width, height
 buttonMainColor = ("white")
@@ -104,37 +107,40 @@ while running:
             # pygame.QUIT event means the user clicked X to close your window
             if event.type == pygame.QUIT:
                 running = False
-              
-            # checks if the user is actively typing something
-            elif activeTyping == False:
+
             
-                if event.type == pygame.MOUSEBUTTONDOWN: # if user clicks their mouse
-                    if event.button == 1: # if the user clicks MB1 (left clicks)
+            elif event.type == pygame.MOUSEBUTTONDOWN: # if user clicks their mouse
 
-                        # loops through each existing textbox to check if one was clicked
-                        for textbox in listTextboxes:
-                            if textbox.collidepoint(event.pos): 
-                                print("textbox clicked")
-                                activeTyping = True
-                                activeTextbox = textbox
-                                
+                # stores the position of the mouse click
+                pos = event.pos
 
-                        # if the main button is clicked on
-                        if buttonMain.collidepoint(event.pos): # if the button collides, at a point, with the event position (which is the click)
-                            print("box successfully clicked!")
-                        
-                        # if the columns button is clicked
-                        elif buttonColumns.collidepoint(event.pos):
-                            buttonColumnsColor = ("dark gray")
-                            activeTyping = True
-                            continue
-    
-    # executes if activeTyping == True, AKA when the user has clicked on a text box to enter a value into
-            #else:
-                
-        # needs to loop through and figure out which text box the user clicked on
-        # best way to do this is likely storing all buttons in a list, then using a for-loop to check if the button collides with click
+                # Boolean for if the user has clicked on a textbox
+                clickedAny = False
+                if event.button == 1: # if the user clicks MB1 (left clicks)
 
+                    # loops through each existing textbox to check if one was clicked
+                    # if textbox is clicked, sets that textbox to be active
+                    for textbox in textboxes:
+
+                        # checks if the mouseclick is on a textbox
+                        if textbox.clickedInside(pos):
+                            textbox.active = True
+                            clickedAny = True
+                        else:
+                            textbox.active = False
+
+                    # if clicked outside a textbox, deactivates all textboxes
+                    if clickedAny == False:
+                        for textbox in textboxes:
+                            textbox.active = False
+
+                    # if the main button is clicked on
+                    if buttonMain.collidepoint(event.pos): # if the button collides, at a point, with the event position (which is the click)
+                        print("box successfully clicked!")
+
+    # if a textbox is active, this will "handle events" AKA update the textbox with user text
+    for textbox in textboxes:
+        textbox.handleEvents(events)
 
     screen.fill("dark blue") # fills the screen with one color to clear it of last frame
 
@@ -145,26 +151,21 @@ while running:
     # the area (dark green) where the matrix boxes will be
     areaMatrix = pygame.draw.rect(screen, areaMatrixColor, areaMatrixCoords)
 
-    # the textbox button for column entry
-    buttonColumns = pygame.draw.rect(screen, buttonColumnsColor, buttonColumnsCoords)
-    if buttonColumns not in listTextboxes:
-        listTextboxes.append(buttonColumns)
-
-
     ### TEXT ### 
     textMain = fontButtons.render("Main Menu", True, "black")
     screen.blit(textMain, (60, 70))
 
     textDimensions = fontDimensions.render("DIMENSIONS: ", True, textDimensionsColor)
     screen.blit(textDimensions, (textDimensionsCoords))
-
-
+       
+    # draws all the textboxes
+    for textbox in textboxes:
+        textbox.draw(screen)
 
     # flip() the display to put  work on screen
     pygame.display.flip()
 
     clock.tick(30)  # limits FPS to 30
-    print(listTextboxes)
-
+    #print(textboxes)
 
 pygame.quit()
